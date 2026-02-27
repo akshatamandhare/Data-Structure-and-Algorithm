c289e7d12fd09c51cbb62861131c1c999428e56c
akshatamandhare <akshatamandharre03@gmail.com>
2026-02-27 11:31:22 +0530

    create new file

---
+++ TCS-Coding-Questions/word_is_the_key.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string keywords[1][16] = {"break", "case", "continue", "default", "defer", "else", "for", "func", "goto", "if", "map", "range", "return", "struct", "type", "var"};
+    string input;
+    cout<<"Enter a string: ";
+    cin>>input;
+    int flag=0;
+    for(int i=0; i<16; i++){
+        if(input==keywords[0][i]){
+            flag=1;
+            break;
+        }
+    }    
+    if(flag==1){
+        cout<<"Input string is a keyword";
+    }
+    else{
+        cout << "Input string is not a keyword";
+    }
+    return 0;
+}
