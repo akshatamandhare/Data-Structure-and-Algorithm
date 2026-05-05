3739aa52d43c4663854c42f03a3261c44faf4c40
akshatamandhare <akshatamandharre03@gmail.com>
2026-05-05 17:01:40 +0530

    add level order

--- Trees/traversal.cpp
+++ Trees/traversal.cpp
@@ -42,10 +42,33 @@ void postorder(Node *root){
     postorder(root->right);
     cout << root->data << " ";
 }
-void levelorder(Node *root){
-    if(root==NULL) return;
+vector<vector<int>> levelorder(Node *root){
 
+    vector<vector<int>> ans;
     
+    if(root==NULL){
+        return ans;
+    }
+
+    queue<Node*>q;
+    q.push(root);
+    while (!q.empty()){
+        int size=q.size();
+        vector<int>level;
+        for(int i=0; i<size; i++){
+            Node *node = q.front();
+            q.pop();
+            if(node->left!=NULL){
+                q.push(node ->left);
+            }
+            if(node->right!=NULL){
+                q.push(node ->right);
+            }
+            level.push_back(node->data);
+        }
+        ans.push_back(level);
+    }
+    return ans;
 }
 
 
@@ -74,7 +97,14 @@ int main(){
     postorder(root);
 
     cout<<endl<<"Levelorder traversal : ";
-    levelorder(root);
+    vector<vector<int>> res = levelorder(root);
+
+    for(auto level : res){
+        for(auto val : level){
+            cout << val << " ";
+        }
+        cout << endl;
+    }
     
     return 0;
 }
